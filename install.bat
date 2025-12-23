a2e7517fdd166ddf0e774fbb755c4752fa9773c3
Petr Stepanov <stepanovps@gmail.com>
2025-12-23 18:05:29 -0500

    update

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 mkdir _vsbuild
 cd _vsbuild
-cmake ../
-msbuild RootTest.sln /p:Configuration=Release
+cmake -DPORTABLE_BUILD=TRUE ../
+msbuild PowerGraphs.sln /p:Configuration=Release
 msbuild INSTALL.vcxproj /p:Configuration=Release
